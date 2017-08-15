d67ac56e709c75725b2b05a677e96d956e526c03
Christian Broberg <cb@webhouse.dk>
2017-08-15 22:25:41 +0200

    Updated eslint rules in boilerplate scripts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
-      </div>
-    )
-  }
+	render() {
+		return (
+            <div>
+                <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+            </div>
+		)
+	}
 }
 
 export default App
